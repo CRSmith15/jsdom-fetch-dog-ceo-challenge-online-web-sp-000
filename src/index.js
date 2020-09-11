6bf15acc45f9d16e7c68fa03a4417d33455c9344
Chance Smith <chancesmith97@hotmail.com>
2020-09-11 20:18:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 
-const imgUrl= "https://dog.ceo/api/breeds/image/random/4" 
+const imgUrl= "https://dog.ceo/api/breeds/image/random/4"
 
 fetch(imgUrl)
 .then(resp = resp.json())
